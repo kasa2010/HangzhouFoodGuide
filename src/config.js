2247d10928ce3f442e83b491928f124f2d99cb0e
kasa2010 <supergrass2000@gmail.com>
2018-04-24 10:12:45 +0800

    修改接口接入方式

---
+++ src/config.js
@@ -0,0 +1,11 @@
+var host = 'https://876379022.supergrass.club'
+
+export default {
+  service: {
+      host,
+      loginUrl: `${host}/weapp/login`,
+      getResUrl: `${host}/weapp/restaurant`,
+      getLikeUrl: `${host}/weapp/like`,
+      getDoneUrl: `${host}/weapp/done`,
+  }
+}
